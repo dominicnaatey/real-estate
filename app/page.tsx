831c4ba3f37f35b46d93e352107b84acdebe41e6
dominicnaatey <dominicnaatey@gmail.com>
2026-03-24 01:00:21 +0000

    move landing page from components to app directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-import LandingPage from "../components/Page";
+"use client";
+
+import { BottomCTA } from "../components/BottomCTA";
+import { Categories } from "../components/Categories";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Hero } from "../components/Hero";
+import { LatestListings } from "../components/LatestListings";
+import { PopularListings } from "../components/PopularListings";
+import { Services } from "../components/Services";
+import { Stats } from "../components/Stats";
+import { Testimonial } from "../components/Testimonial";
 
 export default function Page() {
-  return <LandingPage />;
+  return (
+    <div className="min-h-screen bg-offwhite font-sans">
+      <Header />
+      <main>
+        <Hero />
+        <PopularListings />
+        <Services />
+        <Categories />
+        <LatestListings />
+        <Stats />
+        <Testimonial />
+        <BottomCTA />
+      </main>
+      <Footer />
+    </div>
+  );
 }
