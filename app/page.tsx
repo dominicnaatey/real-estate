d3e0816106ed8ca608ca422b0563c75556a9edda
dominicnaatey <dominicnaatey@gmail.com>
2026-03-24 01:13:57 +0000

    move Header and Footer from page to root layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 
 import { BottomCTA } from "../components/BottomCTA";
 import { Categories } from "../components/Categories";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Navbar";
 import { Hero } from "../components/Hero";
 import { LatestListings } from "../components/LatestListings";
 import { PopularListings } from "../components/PopularListings";
@@ -13,8 +11,7 @@ import { Testimonial } from "../components/Testimonial";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-offwhite font-sans">
-      <Header />
+    <div className="bg-offwhite font-sans">
       <main>
         <Hero />
         <PopularListings />
@@ -25,7 +22,6 @@ export default function Page() {
         <Testimonial />
         <BottomCTA />
       </main>
-      <Footer />
     </div>
   );
 }
