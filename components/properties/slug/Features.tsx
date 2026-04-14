48162b6aff892ea0885c3674beadfedad8a2c24b
dominicnaatey <dominicnaatey@gmail.com>
2026-04-14 11:48:06 +0000

    improve visual hierarchy and spacing

--- components/properties/slug/Features.tsx
+++ components/properties/slug/Features.tsx
@@ -1,3 +1,4 @@
+import { div } from "framer-motion/client";
 import { CheckCircle2 } from "lucide-react";
 
 export type FeaturesProps = {
@@ -28,13 +29,13 @@ export function Features({
 
   return (
     <div className="mt-12">
-      <h3 className="text-lg font-bold text-gray-900 mb-6">{title}</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-16 text-gray-600">
+      <h3 className="text-xl font-bold text-gray-900 mb-6">{title}</h3>
+      <div className="bg-gray-200/60 p-6 rounded-2xl grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-16 text-gray-600">
         {resolvedItems.map((feature) => (
-          <div key={feature} className="flex items-center gap-3">
-            <CheckCircle2 size={20} className="text-[#1E3A8A]" />
-            <span className="text-base">{feature}</span>
-          </div>
+            <div key={feature} className="flex items-center gap-3">
+              <CheckCircle2 size={20} className="text-[#1E3A8A]" />
+              <span className="text-base font-medium text-gray-900">{feature}</span>
+            </div>
         ))}
       </div>
     </div>
