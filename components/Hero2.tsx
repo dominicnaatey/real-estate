0c4986a14c2ad1a4283c23f02235c01f0dedb2ea
dominicnaatey <dominicnaatey@gmail.com>
2026-04-21 16:21:44 +0000

    add comment to filter section in Hero2 component

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -22,7 +22,7 @@ export function Hero2() {
           <h1 className="text-5xl md:text-6xl font-medium text-white mb-6 md:mb-12 tracking-tight text-center">
             Find Your Home
           </h1>
-
+          {/* filter */}
           <div className="mx-auto w-full md:w-auto max-w-5xl">
             <div className="bg-white backdrop-blur-sm p-3 md:p-2 rounded-4xl md:rounded-full shadow-xl border border-black/10 flex flex-col md:flex-row md:items-center gap-3 md:gap-2">
               <div className="flex items-center gap-3 w-full md:w-auto">
