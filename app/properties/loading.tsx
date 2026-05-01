9917f4ce722c81ebf38cfac64df02c91bb6e8bf5
dominicnaatey <dominicnaatey@gmail.com>
2026-05-01 10:11:12 +0000

    remove border and fix aspect ratio class in loading skeleton

--- app/properties/loading.tsx
+++ app/properties/loading.tsx
@@ -2,8 +2,8 @@
 
 function CardSkeleton() {
   return (
-    <div className="rounded-2xl bg-white border border-black/5 overflow-hidden">
-      <div className="aspect-[4/3] bg-gray-100" />
+    <div className="rounded-2xl bg-white overflow-hidden">
+      <div className="aspect-4/3 bg-gray-100" />
       <div className="p-4 space-y-3">
         <div className="h-4 w-3/4 bg-gray-100 rounded" />
         <div className="h-3 w-1/2 bg-gray-100 rounded" />
@@ -41,4 +41,3 @@ export default function Loading() {
     </div>
   );
 }
-
