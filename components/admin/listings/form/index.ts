323dc50daa8a5f832f56ed7d54f2043bc3dbfe04
dominicnaatey <dominicnaatey@gmail.com>
2026-05-13 09:51:05 +0000

    chore(admin/listings/form): export OptionsSection component

--- components/admin/listings/form/index.ts
+++ components/admin/listings/form/index.ts
@@ -4,5 +4,6 @@ export { DescriptionHighlightsSection } from "./DescriptionHighlightsSection";
 export { FeaturesAmenitiesSection } from "./FeaturesAmenitiesSection";
 export { MediaSection } from "./MediaSection";
 export { AgentSection } from "./AgentSection";
+export { OptionsSection } from "./OptionsSection";
 export { useListingFormState } from "./useListingFormState";
 export type { ListingFormMode, ListingFormProps, ListingFormState } from "./types";
